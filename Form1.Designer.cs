1b8cc849ceb603c27c20f4160d952648b1a8a757
Daniel Ajenifuja <335984019@educ.dpcdsb.org>
2023-05-01 22:56:44 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace simon_says
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(177, 110);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(253, 218);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(584, 110);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(272, 218);
+            this.pb2.TabIndex = 1;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(177, 379);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(253, 218);
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(584, 379);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(272, 218);
+            this.pb4.TabIndex = 3;
+            this.pb4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "pb1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "pb2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 419);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "pb3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "pb4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(481, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 659);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace simon_says
+{
+    public partial class Form1 : Form
+    {
+        int[] pattern = { 1, 2, 4, 3, 3, 2, 1, 1, 2, 4 };
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void flash()
+        {
+            foreach (int number in pattern)
+            {
+                switch (number)
+                {
+                    case 1:
+                        pb1.BackColor = Color.Red;
+                        this.Update();
+                        Thread.Sleep(500);
+                        pb1.BackColor = Color.LightGray;
+                        this.Update();
+                        Thread.Sleep(500);
+                        break;
+                    case 2:
+                        pb1.BackColor = Color.Blue;
+                        this.Update();
+                        Thread.Sleep(500);
+                        pb1.BackColor = Color.LightGray;
+                        this.Update();
+                        Thread.Sleep(500);
+                        break;
+                    case 3:
+                        pb1.BackColor = Color.Orange;
+                        this.Update();
+                        Thread.Sleep(500);
+                        pb1.BackColor = Color.LightGray;
+                        this.Update();
+                        Thread.Sleep(500);
+                        break;
+                    case 4:
+                        pb1.BackColor = Color.LimeGreen;
+                        this.Update();
+                        Thread.Sleep(500);
+                        pb1.BackColor = Color.LightGray;
+                        this.Update();
+                        Thread.Sleep(500);
+                        break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flash();
+        }
+    }
+   
+}
+
